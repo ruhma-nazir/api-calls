3ccc9f907f77f0cfe6fb20c264470acc2fbb1b68
ruhma-nazir <ruhma.nazir@devsinc.com>
2025-07-22 17:16:12 +0500

    spelling correct

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ load_dotenv()
 model = ChatOpenAI(model='gpt-4')
 
 st.header("My Chat Modelgit status ")
-papertype = st.selectbox("Select the documentary",["Scotland: A Wild Year","The Story of Scotland","Scotland: Rome’s Final Frontier","Scotland’s Greatest Escape","ecrets of Great British Castles","Scotlands Greatest Castles","Castles: Britain’s Fortified History"])
+papertype = st.selectbox("Select the documentary",["Scotland: A Wild Year","The Story of Scotland","Scotland: Rome’s Final Frontier","Scotland’s Greatest Escape","secrets of Great British Castles","Scotlands Greatest Castles","Castles: Britain’s Fortified History"])
 styleType = st.selectbox("Select the explanation type",["Example-Based","Analytical","Technical","StoryTelling"])
 lengthInput = st.selectbox("Select the length:",["Short","Detailed","Indepth","Brief",])
 
