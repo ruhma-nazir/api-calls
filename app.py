1883a81d2b2c7be2a53de0c77f986e966393e52c
ruhma-nazir <ruhma.nazir@devsinc.com>
2025-07-22 15:22:42 +0500

    app name changed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 load_dotenv()
 model = ChatOpenAI(model='gpt-4')
 
-st.header("Chat Box")
+st.header("My Chat Modelgit status ")
 papertype = st.selectbox("Select the documentary",["Scotland: A Wild Year","The Story of Scotland","Scotland: Rome’s Final Frontier","Scotland’s Greatest Escape","ecrets of Great British Castles","Scotlands Greatest Castles","Castles: Britain’s Fortified History"])
 styleType = st.selectbox("Select the explanation type",["Example-Based","Analytical","Technical","StoryTelling"])
 lengthInput = st.selectbox("Select the length:",["Short","Detailed","Indepth","Brief",])
